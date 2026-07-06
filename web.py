2122b4c603bd77c6227f3a8cafc2cd00ca6302c2
Jashan Sambyal <sambyaljashan@gmail.com>
2026-07-06 20:20:38 +0530

    Fixes Some Bug

--- web.py
+++ web.py
@@ -5,9 +5,13 @@ import functions
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state['new_todo'] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
+    todo = st.session_state['new_todo'].strip()
+
+    if todo:
+        todos.append(todo)
+        functions.write_todos(todos)
+
+    st.session_state["new_todo"] = ""
 
 st.title("My todo app 📝")
 st.caption("Stay organized and get things done!")
